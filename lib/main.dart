6dc5985a7afe030d467ab8632f9be2e16f62d557
Asdzxc2595 <dudeenakab@gmail.com>
2024-01-12 17:11:33 +0700

    upload second time.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'อีย่าาาาา',theme: ThemeData(primaryColor: Colors.amber),
+      title: 'อีย่าาาาาาาา',theme: ThemeData(primaryColor: Colors.amber),
       home: homepages(),
     );
   }
